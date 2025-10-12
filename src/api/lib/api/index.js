c4fbe702753db8c61a2efdcf26701b704c327cb9
lewisjackson704-lang <lewisjackson704@gmail.com>
2025-10-12 09:50:22 -0400

    Add centralized exports for API modules

--- src/api/lib/api/index.js
+++ src/api/lib/api/index.js
@@ -1,1 +1,7 @@
+// src/api/lib/api/index.js
 
+// Centralized Exports for API Modules
+export * from "../../auth";   // Auth-related API functions
+export * from "../../videos"; // Video-related API functions
+export * from "../../streams"; // Stream-related API functions
+export * from "../../fans";   // Fan-related API functions
